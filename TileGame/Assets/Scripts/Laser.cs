d854e6579f11c579ce7346fd01465d2f7b22af25
Parker Bennion <parkerbennion@gmail.com>
2022-03-11 17:06:44 -0700

    Added laser and started targeting script

--- TileGame/Assets/Scripts/Laser.cs
+++ TileGame/Assets/Scripts/Laser.cs
@@ -5,18 +5,20 @@ public class Laser : MonoBehaviour
 {
     private void Update()
     {
-        int layerMask = 1 << 8;
+        //int layerMask = 1 << 8;
 
         RaycastHit hit;
-        if (Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out hit,Mathf.Infinity,layerMask))
+        if (Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out hit,Mathf.Infinity))
         {
             Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.forward)*hit.distance,Color.cyan);
-            Debug.Log("did Hit");
+            //Debug.Log("did Hit");
+            hit.collider.GetComponent<Renderer>().material.color = Color.black;
+            Instantiate(ScriptableObject.CreateInstance<SO_Variables>());
         }
         else
         {
             Debug.DrawRay(transform.position,transform.TransformDirection(Vector3.forward)*1000,Color.red);
-            Debug.Log("did not hit");
+            //Debug.Log("did not hit");
         }
     }
 }
